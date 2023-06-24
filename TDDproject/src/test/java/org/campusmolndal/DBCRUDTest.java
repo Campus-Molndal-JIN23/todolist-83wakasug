0915f4a50d50450b83a405059a61cd72741247f9
83waksug <119501535+83wakasug@users.noreply.github.com>
2023-06-24 11:42:05 +0200

    test

--- TDDproject/src/test/java/org/campusmolndal/DBCRUDTest.java
+++ TDDproject/src/test/java/org/campusmolndal/DBCRUDTest.java
@@ -6,21 +6,58 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 class DBCRUDTest {
 PreparedStatement mockpstm;
+Connection mockConn;
+ResultSet mockRst;
+SQLite mockSqlite;
+Statement mockStm;
+ResultSet mockResultSet;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws SQLException {
+        String query = "test";
         Mockito.framework().clearInlineMocks();
         mockpstm = Mockito.mock(PreparedStatement.class);
+        mockConn = Mockito.mock(Connection.class);
+        mockRst = Mockito.mock(ResultSet.class);
+        mockSqlite = Mockito.mock(SQLite.class);
+        mockResultSet = Mockito.mock(ResultSet.class);
+        mockStm = Mockito.mock(Statement.class);
+        mockStatic(DriverManager.class);
+        when(DriverManager.getConnection(Mockito.anyString())).thenReturn(mockConn);
+        when(mockSqlite.connection()).thenReturn(mockConn);
+        when(mockConn.prepareStatement(query)).thenReturn(mockpstm);
+        //when(mockStm.executeQuery(query)).thenReturn(any());
+        when(mockStm.executeQuery(anyString())).thenReturn(mockResultSet);
+        when(mockpstm.executeUpdate()).thenReturn(1);
+
+    }
+
+    @Test
+    void checkIfmockpstmAndResultSetRuns() throws SQLException {
+        String query = "test";
+        int expected = 1;
+        mockpstm = mockConn.prepareStatement(query);
+        mockRst = mockpstm.executeQuery();
+        int actual =mockpstm.executeUpdate();
+        verify(mockConn,times(1)).prepareStatement(query);
+        verify(mockpstm,times(1)).executeQuery();
+
+        assertEquals(expected,actual);
+    }
+    void setUpHashMap(){
+
+
     }
 
 
