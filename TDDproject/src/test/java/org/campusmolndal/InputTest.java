db388e7b6c0c02828536eee8bb65ad86c3d6ae97
Wakana <119501535+83wakasug@users.noreply.github.com>
2023-06-25 01:13:35 +0200

    Add files via upload

--- TDDproject/src/test/java/org/campusmolndal/InputTest.java
+++ TDDproject/src/test/java/org/campusmolndal/InputTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.internal.configuration.injection.scanner.MockScanner;
 
@@ -13,53 +14,34 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
 class InputTest {
-    private InputStream inputStream;
-    //Reference https://stackoverflow.com/questions/76239380/how-to-take-predefined-user-input-for-unit-tests-in-java
-
-    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-    private final PrintStream originalOut = System.out;
-    private final PrintStream originalErr = System.err;
-
-    @BeforeEach
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outputStream));
-        System.setErr(new PrintStream(errContent));
-    }
-
-    @AfterEach
-    public void restoreStreams() {
-        System.setOut(originalOut);
-        System.setErr(originalErr);
-    }
-
-
     @Test
     void testStr() {
-        String testInput = "Katt";
-        inputStream = new ByteArrayInputStream(testInput.getBytes());
-        System.setIn(inputStream);
-        String expected = "Katt";
-        String actual = Input.Str();
-        assertEquals(expected, actual);
+        String expected = "test";
+        try (MockedStatic<Input> input = Mockito.mockStatic(Input.class)) {
+            input.when(Input::Str).thenReturn(expected);
+            String actual = Input.Str();
+            assertEquals(expected, actual);
+        }
     }
 
      //TODO fixTest
    @Test
     void testNumber() {
-       String number = String.valueOf(1);  // Convert int to String
-       ByteArrayInputStream inputStream = new ByteArrayInputStream(number.getBytes());
-       System.setIn(inputStream);
-       int expected = Integer.parseInt(number);
-       int actual = Input.number();
-       assertEquals(expected, actual);
+       int expected = 5;
+       try (MockedStatic<Input> input = Mockito.mockStatic(Input.class)) {
+           input.when(Input::number).thenReturn(expected);
+           int actual = Input.number();
+           assertEquals(expected, actual);
+       }
     }
 
 }
